3ca821ca7913b7145c7ef8b84d8be133b160abb0
JpLeon1 <juanpabloleonpineda@gmail.com>
2026-06-13 10:36:55 -0500

    se modifica codigo de politicas.html

--- server.js
+++ server.js
@@ -68,7 +68,7 @@ app.post('/LoginServlet', (req, res) => {
             
             if (usuarioEncontrado.PASSWORD === password) {
                 console.log(`¡Inicio de sesión exitoso! Bienvenido, ${usuarioEncontrado.NOMBRE}`);
-                // Recuerda tener tu archivo Solicitud_Credito.html creado en tu carpeta
+              
                 res.redirect('/Solicitud_de_credito.html'); 
                 
             } else {
