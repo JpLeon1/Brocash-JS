0e30bc726b0a3bf96f2d8fb42138ba9426071f5e
JpLeon1 <juanpabloleonpineda@gmail.com>
2026-06-22 21:04:17 -0500

    cambio debido a diferencia en la DB

--- models/usuarioModel.js
+++ models/usuarioModel.js
@@ -1,21 +1,20 @@
 // models/usuarioModel.js
-const db = require('../config/db'); // Conexión centralizada
+const db = require('../config/db'); 
 
 const Usuario = {
-    // Función para buscar por cédula (Usada en el Login)
+    // Función para buscar un usuario por su cédula
     buscarPorCedula: (cedula, callback) => {
         const query = 'SELECT * FROM REGISTRO_USUARIO WHERE ID_USUARIO = ?';
         db.query(query, [cedula], callback);
     },
 
-    // Función para insertar un nuevo usuario (Usada en el Registro)
     crear: (datosUsuario, callback) => {
         const query = 'INSERT INTO REGISTRO_USUARIO (ID_USUARIO, NOMBRE, APELLIDO, EDAD, EMAIL, TELEFONO, PASSWORD) VALUES (?, ?, ?, ?, ?, ?, ?)';
         db.query(query, [
             datosUsuario.Cedula,
             datosUsuario.Nombre,
-            "N/A", // Apellido por defecto
-            18,    // Edad por defecto
+            "N/A", 
+            18,    
             datosUsuario.email,
             datosUsuario.telefono,
             datosUsuario.password
@@ -23,4 +22,5 @@ const Usuario = {
     }
 };
 
+module.exports = Usuario;
 module.exports = Usuario;
