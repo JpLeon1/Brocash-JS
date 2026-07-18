8cf810ee6cdf5c3309ca444ab22046b859c02af3
JpLeon1 <juanpablo30_10@hotmail.com>
2026-07-18 00:27:29 -0500

    se deja comentario de DB

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const db = require('./config/db.js');
+//const db = require('./config/db.js');
 
 // ==========================================
 // 1. IMPORTACIÓN DE CONTROLADORES 
