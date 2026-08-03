613878dbf5760dddcd58f156ab9e8005b28b6a51
JpLeon1 <juanpablo30_10@hotmail.com>
2026-08-03 13:08:20 -0500

    Chicos realice una limpieza general en mi Pc y reinstale algunos programas para verificar que queden funcionando bien  ( XAMPP Y MYSQL para la base de Datos

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '6406',
+    password: '',
     database: 'Brocash'
 });
 
